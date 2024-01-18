1a9a6a804cadbb22e7c1bf425fb643cd650226c2
stewyman <stewyman@REEDSTEWARTE3F6>
2024-01-17 23:39:46 -0700

    Finished coding, added dice throwing characters.

--- Program.cs
+++ Program.cs
@@ -9,6 +9,11 @@ internal class Program
         int rolls = 0;
 
         System.Console.WriteLine("Welcome to the dice throwing simulator!");
+        System.Console.WriteLine(" (0_0)                  ");
+        System.Console.WriteLine(" \\ | __         ___    ");
+        System.Console.WriteLine("  \\|/   '      (._.)        ");
+        System.Console.WriteLine("   |      '     <|>         ");
+        System.Console.WriteLine("  / \\    o o    _/\\_         ");
         System.Console.WriteLine("How many dice rolls would you like to simulate?");
 
         rolls = int.Parse(System.Console.ReadLine());
--- Roll.cs
+++ Roll.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,7 +9,7 @@ namespace DiceGame
 {
     internal class Roll
     {
-        List<int> DiceTotal = new List<int>();
+        private int[] DiceTotal = new int[11];
 
         // Constructor
         public Roll(int num)
@@ -18,7 +19,7 @@ namespace DiceGame
 
         public void RollLoop(int num)
         {
-            DiceTotal.Clear();
+            // DiceTotal.Clear();
             // instance of random
             Random random = new Random();
 
@@ -30,15 +31,33 @@ namespace DiceGame
                 int dice2 = random.Next(1, 7);
                 int sum = dice1 + dice2;
 
-                // for look places a 0 in each place in list
+                // System.Console.WriteLine(DiceTotal.Count);
+                // Update the counter for the current sum
                 if (sum >= 2 && sum <= 12)
                 {
-                    DiceTotal[sum - 2]++;
+                    DiceTotal[sum - 2] = DiceTotal[sum - 2] + 1;
                 }
-            }   
-            
-                // Print the results of dice rolls
-                System.Console.WriteLine("Results of dice rolls: " + string.Join(", ", DiceTotal));
+            }
+
+            // Print the results of dice rolls in graph
+            System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+            System.Console.WriteLine("Each \"*\" represents 1% of the total number of dice rolls");
+            System.Console.WriteLine($"Total number of rolls = {num}");
+            System.Console.WriteLine(" ");
+
+            int index = 2;
+            while (index <= 12)
+            {
+                // Find the percentage of the combination occurrence. Convert to string to convert to double.
+                double percentage = (DiceTotal[index - 2] / (double)num) * 100;
+                int numast = (int)Math.Round(percentage);
+
+                string space = index >= 2 && index <= 9 ? " " : "";
+                System.Console.WriteLine($"{space}{index}: {new string('*', numast)}");
+                index++;
+            }
+            System.Console.WriteLine(" ");
+            System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
         }
     }
 }
